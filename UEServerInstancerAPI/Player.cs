db64298837ed2d6248c539dffe58be0697473784
Brandon Zeier <43791891+bzeier@users.noreply.github.com>
2023-07-03 01:15:50 +0200

    changes

---
+++ UEServerInstancerAPI/Player.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml;
+
+namespace UEServerInstancerAPI
+{
+    public class Player
+    {
+        public int PlayerUniqueID = 0;
+        public bool isActive = false;
+    }
+}
--- UEServerInstancerAPI/Server.cs
+++ UEServerInstancerAPI/Server.cs
@@ -1,18 +1,40 @@
-﻿namespace UEServerInstancerAPI
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace UEServerInstancerAPI
 {
     public class Server
     {
-        public bool isRunning = false;
+        public Process process;
+        public Player[] players = new Player[] { };
 
         public bool Run()
         {
-            isRunning = true;
+
+            //Debug.WriteLine(
+            string path = AppDomain.CurrentDomain.BaseDirectory;
+            Debug.WriteLine(path);
+            string[] files = Directory.GetFiles(path);
+
+            process = Process.Start(path + "/FL64.exe");
+
             return true;
+        }
+
+        public bool GetIsRunning()
+        {
+            return process.HasExited;
+        }
+
+        public void AddPlayer(Player player)
+        {
+            player.isActive = true;
+            players.Append(player);
         }
 
         public bool ShutDown()
         {
-            isRunning = false;
+            process.Kill();
             return true;
         }
     }
